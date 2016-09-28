e0d88e5564ebdae3104da21ff30581414bda2fc8
boris.liu <boris_liu@foxmail.com>
2016-09-28 10:33:05 +0800

    add linux command
    Change-Id: I275aa88d17616ff4790b2d6c846c8bd200968b45

--- src/com/manager/platform/ProcessManagerLinux.java
+++ src/com/manager/platform/ProcessManagerLinux.java
@@ -11,12 +11,15 @@ public class ProcessManagerLinux extends ProcessManager {
 	@Override
 	public int getProcessMemoryUsed() {
 		// TODO Auto-generated method stub
+		//top -p pid 查看程序的情况 
+		//cat /proc/pid/status 
 		return 0;
 	}
 
 	@Override
 	public void closeProcess() {
 		// TODO Auto-generated method stub
+		//KILL -9 ID  //-9强制关
 
 	}
 
